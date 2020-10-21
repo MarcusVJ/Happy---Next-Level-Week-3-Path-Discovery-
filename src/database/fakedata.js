c5c2fda564a7df389e400698c2c341c32983cade
Marcus Vinicius de Jesus <marcus.vj@live.com>
2020-10-21 00:36:20 -0300

    workshop 04

---
+++ src/database/fakedata.js
@@ -0,0 +1,42 @@
+module.exports = [
+  {
+    id: 1,
+    name: 'Lar das Meninas',
+    lat: '-27.222633',
+    lng: '-49.6455874',
+    description:
+      'Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.',
+    images: [
+      'https://source.unsplash.com/random?id1',
+      'https://source.unsplash.com/random?id2',
+      'https://source.unsplash.com/random?id3',
+      'https://source.unsplash.com/random?id4',
+      'https://source.unsplash.com/random?id5',
+      'https://source.unsplash.com/random?id6',
+    ],
+    instructions:
+      'Venha como se sentir a vontade e traga muito amor e paciência para dar.',
+    opening_hours: 'Horário de visitas Das 18h até 8h',
+    open_on_weekends: '1',
+  },
+  {
+    id: 1,
+    name: 'Lar das Meninos',
+    lat: '-27.222633',
+    lng: '-49.6555874',
+    description:
+      'Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.',
+    images: [
+      'https://source.unsplash.com/random?id1',
+      'https://source.unsplash.com/random?id2',
+      'https://source.unsplash.com/random?id3',
+      'https://source.unsplash.com/random?id4',
+      'https://source.unsplash.com/random?id5',
+      'https://source.unsplash.com/random?id6',
+    ],
+    instructions:
+      'Venha como se sentir a vontade e traga muito amor e paciência para dar.',
+    opening_hours: 'Horário de visitas Das 18h até 8h',
+    open_on_weekends: '0',
+  },
+];
